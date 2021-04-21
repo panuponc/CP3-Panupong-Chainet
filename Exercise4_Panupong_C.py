982563f0005fcf0c9328590fa8cc60a00c4027a3
panuponc <panupong_c@outlook.co.th>
2021-04-21 14:53:01 +0700

    Create Exercise4_Panupong_C.py
    การบ้าน Exercise 4

---
+++ Exercise4_Panupong_C.py
@@ -0,0 +1,12 @@
+#การบ้าน มีรายวิชา 4 วิชา สร้างตัวแปรเก็บค่าคะแนนในแต่ละวิชา เป็นทศนิยมก็ได้ แล้วแสดงผล ลุย !
+FouScore = 80.0
+GenScore = 90.5
+IntScore = 77.5
+ComScore = 100.0
+
+print("--- Your Score ---")
+print("Foundation English : ",FouScore," Point" )
+print("General Business : ",GenScore," Point")
+print("Introduction to Computer Systems : ",IntScore," Point")
+print("Computer Programming : ",ComScore," Point")
+print("--- Good Job ---")
