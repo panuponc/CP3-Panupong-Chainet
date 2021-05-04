883ab30a2ba9d414ddc6cca53212c850cdea807f
panuponc <panupong_c@outlook.co.th>
2021-05-04 10:52:55 +0700

    Create Exercise5_1_Panupong_C.py

---
+++ Exercise5_1_Panupong_C.py
@@ -0,0 +1,10 @@
+a = int(input("Your FirstNum : "))
+b = int(input("Your SecondNum : "))
+c1 = a + b
+c2 = a - b
+c3 = a * b
+c4 = a / b
+print(a ,"+" , b , "=" , c1)
+print(a ,"-" , b , "=" , c2)
+print(a ,"*" , b , "=" , c3)
+print(a ,"/" , b , "=" , c4)
